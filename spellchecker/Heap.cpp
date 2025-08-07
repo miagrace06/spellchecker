51a4ad29b5542972ec4207dcb5944b4e7a96d61c
miagrace06 <mcampanella@ucsb.edu>
2025-08-07 11:40:21 -0700

    Update Heap.cpp

--- spellchecker/Heap.cpp
+++ spellchecker/Heap.cpp
@@ -1,17 +1,65 @@
 #include "Heap.h"
-#include <utility> 
-#include <string>  
+#include <stdexcept>
+#include <utility>
 
-Heap::Heap() {}
+Heap::Heap(size_t capacity) : mCapacity(capacity), mCount(0) {
+    mData = new Entry[mCapacity];
+}
 
-void Heap::push(std::pair<std::string, double> newValue) {
-    heap.push_back(newValue);
-    int index = heap.size() - 1;
+Heap::Heap(const Heap& other) : mCapacity(other.mCapacity), mCount(other.mCount) {
+    mData = new Entry[mCapacity];
+    for (size_t i = 0; i < mCount; ++i) {
+        mData[i] = other.mData[i];
+    }
+}
 
+Heap::Heap(Heap&& other) : mData(other.mData), mCapacity(other.mCapacity), mCount(other.mCount) {
+    other.mData = nullptr;
+    other.mCapacity = 0;
+    other.mCount = 0;
+}
+
+Heap::~Heap() {
+    delete[] mData;
+}
+
+size_t Heap::capacity() const {
+    return mCapacity;
+}
+
+size_t Heap::count() const {
+    return mCount;
+}
+
+const Heap::Entry& Heap::lookup(size_t index) const {
+    if (index >= mCount) {
+        throw std::out_of_range("Index out of range");
+    }
+    return mData[index];
+}
+
+const Heap::Entry& Heap::top() const {
+    if (mCount == 0) {
+        throw std::underflow_error("Heap is empty");
+    }
+    return mData[0];
+}
+
+void Heap::push(const std::string& value, float score) {
+    if (mCount >= mCapacity) {
+        throw std::overflow_error("Heap is full");
+    }
+    
+    mData[mCount].value = value;
+    mData[mCount].score = score;
+    
+    size_t index = mCount;
+    mCount++;
+    
     while (index > 0) {
-        int parent = (index - 1) / 2;
-        if (heap[index].second < heap[parent].second) {
-            std::swap(heap[index], heap[parent]);
+        size_t parent = (index - 1) / 2;
+        if (mData[index].score < mData[parent].score) {
+            std::swap(mData[index], mData[parent]);
             index = parent;
         } else {
             break;
@@ -19,43 +67,60 @@ void Heap::push(std::pair<std::string, double> newValue) {
     }
 }
 
-std::pair<std::string, double> Heap::pop() {
-    if (heap.empty()) {
-        return std::make_pair("", -1.0);
+Heap::Entry Heap::pop() {
+    if (mCount == 0) {
+        throw std::underflow_error("Heap is empty");
     }
-
-    std::pair<std::string, double> minElement = heap[0];
-    heap[0] = heap.back();
-    heap.pop_back();
-
-    percolateDown(0);
-    return minElement;
+    
+    Entry result = mData[0];
+    
+    mCount--;
+    if (mCount > 0) {
+        mData[0] = mData[mCount];
+        percolateDown(0);
+    }
+    
+    return result;
 }
 
-void Heap::percolateDown(int index) {
-    int size = heap.size();
+Heap::Entry Heap::pushpop(const std::string& value, float score) {
+    if (mCount == 0) {
+        throw std::underflow_error("Heap is empty");
+    }
+    
+    Entry result = mData[0];
+    
+    mData[0].value = value;
+    mData[0].score = score;
+    percolateDown(0);
+    
+    return result;
+}
 
+void Heap::percolateDown(size_t index) {
     while (true) {
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-        int smallest = index;
-
-        if (left < size && heap[left].second < heap[smallest].second) {
+        size_t left = index * 2 + 1;
+        size_t right = index * 2 + 2;
+        size_t smallest = index;
+        
+        if (left < mCount && mData[left].score < mData[smallest].score) {
             smallest = left;
         }
-        if (right < size && heap[right].second < heap[smallest].second) {
+        if (right < mCount && mData[right].score < mData[smallest].score) {
             smallest = right;
         }
-
+        
+        if (left < mCount && right < mCount && 
+            mData[left].score == mData[right].score && 
+            mData[left].score < mData[index].score) {
+            smallest = left;
+        }
+        
         if (smallest != index) {
-            std::swap(heap[index], heap[smallest]);
+            std::swap(mData[index], mData[smallest]);
             index = smallest;
         } else {
             break;
         }
     }
-}
-
-bool Heap::isEmpty() const {
-    return heap.empty();
 }
